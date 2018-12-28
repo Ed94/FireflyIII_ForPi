8e5744d0a6737532cc7be8d52de2cb581032c965
Ed94 <edwardgz@gmail.com>
2018-12-28 17:51:16 -0500

    fixed checkout not working as intended in getRepo.sh

--- Server/getRepo.sh
+++ Server/getRepo.sh
@@ -15,5 +15,4 @@ git init .
 git remote add origin https://github.com/Ed94/FireflyIII_ForPi.git
 git fetch --all
 git reset --hard origin/RaspberryPi-3-B+
-git checkout -t -f RaspberryPi-3-B+
-git checkout RaspberryPi-3-B+
+git checkout -t -f -b RaspberryPi-3-B+
