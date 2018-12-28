d79e4803ebca7b79c1103a9365df3b0e932403b3
Ed94 <edwardgz@gmail.com>
2018-12-28 17:48:52 -0500

    fixed getRepo not being in the correct directory.

--- Server/getRepo.sh
+++ Server/getRepo.sh
@@ -4,6 +4,13 @@
 #
 # Gets the firefly 3 repository and checks out the pi branch.
 
+set -x
+
+User="Eds_FireflyPi"
+FireflyIII_Directory="/home/$User/Docker/FireflyIII"
+
+cd $FireflyIII_Directory
+
 git init .
 git remote add origin https://github.com/Ed94/FireflyIII_ForPi.git
 git fetch --all
