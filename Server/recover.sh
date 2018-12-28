895967ac60305a0a5c2e7dc075f0f940308ca630
Ed94 <edwardgz@gmail.com>
2018-12-28 16:51:48 -0500

    more updates to git related portions of recover script.

--- Server/recover.sh
+++ Server/recover.sh
@@ -22,8 +22,9 @@ cd $FireflyIII_Directory
 
 git init .
 git remote add origin https://github.com/Ed94/FireflyIII_ForPi.git
-git fetch
-git checkout -t -f origin/RaspberryPi-3-B+
+git fetch --all
+git reset --hard origin/RaspberryPi-3-B+
+git checkout -t -f RaspberryPi-3-B+
 
 cp $BackupDirectory/Dockerfile $SourceDirectory
 cp $BackupDirectory/docker-compose.yml $ServerDirectory
